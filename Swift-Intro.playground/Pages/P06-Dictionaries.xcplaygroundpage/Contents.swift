74fee0a66a8331b0cdc75570810a3aaa04aeec97
Yujin Ariza <yujin.ariza@gmail.com>
2016-04-26 12:26:44 -0400

    initial commit

---
+++ Swift-Intro.playground/Pages/P06-Dictionaries.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+/*:
+ # Dictionaries
+ A Dictionary is Collection data structure which stores multiple values, where each value has a unique key associated with it. (If you know Python, you may be familiar with Python dictionaries; in Java these are called HashMaps.) Unlike arrays, dictionaries do not keep the values in any order. Dictionaries are helpful in a situation where you want to quickly look up a value based on its unique key.
+ 
+ Let's jump right in and define a dictionary variable.
+ */
+
+var cities: [String :  String] = ["New York City" : "USA", "Paris" : "France", "London" : "UK"]
+
+/*:
+ As you can see, the type of a dictionary depends on the type of its key and the type of its value. The general dictionary type is defined as [<key type> : <value type>]. In the example above, the key and the value are both Strings. The key represents the name of the city, and the value represents the country the city is in.
+ Just like arrays, the type of the dictionary can be inferred, so "[String : String]" is not necessary.
+ 
+ To count the number of key-value pairs, you can use the `count` property just like arrays. `isEmpty` works as well.
+ */
+print("The dictionary contains \(cities.count) items.")
+
+/*:
+ You can add a new key-value pair like this:
+ */
+cities["San Francisco"] = "USA"
+/*:
+ It's similar to putting a value in an array, except instead of putting the index number inside the brackets, you put the key.
+ 
+ You can also the change the value that a key is associated with in the same manner:
+ */
+cities["San Francisco"] = "United States of America"
+
+/*:
+ When trying to retrieve a value for a key, there is a possibility that key-value pair does not exist, so you have to make sure you check for this case. As a result, it is best to use optional binding to retrieve a value from the dictionary:
+ */
+if let country = cities["London"] {
+    print("London is in \(country).")
+}
+else {
+    print("The dictionary does not contain London as a key.")
+}
+/*:
+ You can remove a key-value pair simply by setting the key's value to nil:
+ */
+cities["London"] = nil
+print(cities) //does not contain "London" anymore
+/*:
+ To empty the dictionary:
+ */
+cities = [:]
+/*:
+ To define a new empty dictionary:
+ */
+var dictionary = [String : Int]()
+/*:
+ Notice how the type of the values is Int. The value type can be any type you want. The key can also be any type you want. However, the key has to be a type that provides a value for its `hashValue` property. This is out of the scope of this tutorial, and there are rarely any situations that you will need a custom type to be the key type for a dictionary.
+ */
+
+
+
+
+
---
+++ Swift-Intro.playground/Pages/P07-Control Flow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,110 @@
+/*:
+ # Control Flow
+ In this tutorial, we'll cover how to use the following statments in Swift: `if`, `for`, `while`, `switch`.
+ 
+ ## `if` statments
+ A simple `if` statment in Swift looks like the following:
+ */
+
+let isHuman = true
+
+if isHuman {
+    print("I'm a human")
+} else {
+    print("Non human")
+}
+
+//: Note that parentheses are optional for `if` statments in Swift.
+
+/*:
+ ## `for` Loops
+ There are two types of `for` loops in Swift. Apple explains the two as:
+ * The `for-in` loop performs a set of statements for each item in a sequence.
+ * The `for` loop performs a set of statements until a specific condition is met, typically by incrementing a counter each time the loop ends.
+ */
+
+//: ### `for-in`
+
+let cities = ["New York", "Paris", "London", "Shanghai"]
+
+for city in cities {
+    print("I live in \(city)")
+}
+
+/*:
+ The `for-in` loop is similar to a Python `for` loop or a Java `enhanced for` loop.
+ 
+ We can also use a `for-in` with a range of numbers:
+ */
+
+for index in 1...3 {
+    print("I can count to \(index)")
+}
+
+/*:
+ ### `for`
+ You can also define for loops in the following format:
+ */
+
+for var index = 0; index < 3; index++ {
+    print("I can count to \(index)")
+}
+/*:
+ This looks more like a standard Java for loop.
+ The format is as follows:
+ 
+ for initialization; condition; increment {
+ statements
+ }
+ */
+
+/*:
+ ## `while` Loops
+ A `while` loop keeps running as long as its condition is `true`.
+ */
+
+var result = 1
+
+while result < 60 {
+    result *= 5
+}
+
+print(result)
+
+/*:
+ `result` will be updated as follows: 1, 5, 25, 50, 125. Notice that the loop ran 4 times, but it stopped once `result` became more than 60.
+ 
+ The format for `while` loops is:
+ 
+ while condition {
+ statements
+ }
+ */
+
+/*:
+ ## `switch` statement
+ A switch statement allows you to define what happens for specific values of a variable.
+ Here, we decide what to do based on what `grade` is. We could do the same thing with lots of `if` statements, but the `switch` syntax is more compact and easier to read.
+ */
+
+let grade = "A"
+
+switch grade {
+case "A":
+    print("Excellent grade")
+case "B":
+    print("Good grade")
+case "C":
+    print("Passing grade")
+default:
+    print("Unknown grade")
+}
+/*:
+ Two important things you should keep in mind when using `switch` in Swift:
+ * You don't need to add a `break` statment after every condition. (Yay!)
+ * The `default` case is like an `else` case that would run if non of the previous conditions are met. Every `switch` statment must have a `default` case.
+ */
+
+/*:
+ For more details on what we covered here, please visit [Apple's control flow guide.](https://developer.apple.com/library/prerelease/ios/documentation/Swift/Conceptual/Swift_Programming_Language/ControlFlow.html#//apple_ref/doc/uid/TP40014097-CH9-ID120)
+ */
