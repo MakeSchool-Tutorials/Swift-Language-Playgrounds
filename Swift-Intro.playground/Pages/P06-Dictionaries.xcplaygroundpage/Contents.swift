152358651e4f5ebac62f52e8580ade0a1120fb00
Daniel Haaser <thedapperdan@gmail.com>
2016-06-04 20:43:23 -0700

    content and formatting changes to P06

--- Swift-Intro.playground/Pages/P06-Dictionaries.xcplaygroundpage/Contents.swift
+++ Swift-Intro.playground/Pages/P06-Dictionaries.xcplaygroundpage/Contents.swift
@@ -1,21 +1,26 @@
 /*:
  # Dictionaries
- A Dictionary is Collection data structure which stores multiple values, where each value has a unique key associated with it. (If you know Python, you may be familiar with Python dictionaries; in Java these are called HashMaps.) Unlike arrays, dictionaries do not keep the values in any order. Dictionaries are helpful in a situation where you want to quickly look up a value based on its unique key.
  
+ Dictionary is a collection type that can store multiple values, and each value has a unique key associated with it. (If you know Python, you may be familiar with Python dictionaries; in Java these are called HashMaps.) Unlike arrays, dictionaries are _unordered_, which means they do not keep the values in any particular order. Dictionaries are helpful in situations where you want to quickly look up a value based on its unique key.
+ 
+ ## Instantiation
  Let's jump right in and define a dictionary variable.
  */
-
 var cities: [String: String] = ["New York City": "USA", "Paris": "France", "London": "UK"]
-
 /*:
- As you can see, the type of a dictionary depends on the type of its key and the type of its value. The general dictionary type is defined as [<key type> : <value type>]. In the example above, the key and the value are both Strings. The key represents the name of the city, and the value represents the country the city is in.
- Just like arrays, the type of the dictionary can be inferred, so "[String : String]" is not necessary.
+ As you can see, the type of a dictionary depends on both the type of its key and the type of its value. The syntax to declare a dictionary type is `[<key type>: <value type>]`. In the example above, the key and the value are both `String`. In this case, the key is the name of a city, and the value is the country that the city is in.
+ Just like arrays, the type of the dictionary can be inferred, so the `[String : String]` part is not strictly necessary.
  
+ Also like arrays, you can both instantiate and place items in a dictionary using a literal, in this case a _dictionary literal_, which you can see on the right-hand side of the expression. As you can see, place a colon `:` between each key and its corresponding value, and use a comma `,` to separate each key-value pair.
+ */
+/*:
+## Examine the Dictionary
  To count the number of key-value pairs, you can use the `count` property just like arrays. `isEmpty` works as well.
  */
 print("The dictionary contains \(cities.count) items.")
-
+/*: */
 /*:
+ ## Using the Dictionary
  You can add a new key-value pair like this:
  */
 cities["San Francisco"] = "USA"
@@ -25,14 +30,12 @@ cities["San Francisco"] = "USA"
  You can also the change the value that a key is associated with in the same manner:
  */
 cities["San Francisco"] = "United States of America"
-
 /*:
  When trying to retrieve a value for a key, there is a possibility that key-value pair does not exist, so you have to make sure you check for this case. As a result, it is best to use optional binding to retrieve a value from the dictionary:
  */
 if let country = cities["London"] {
     print("London is in \(country).")
-}
-else {
+} else {
     print("The dictionary does not contain London as a key.")
 }
 /*:
@@ -44,12 +47,15 @@ print(cities) //does not contain "London" anymore
  To empty the dictionary:
  */
 cities = [:]
+//: - note: `[:]` is the literal for empty dictionary, it can't be `[]` because that means empty array
 /*:
  To define a new empty dictionary:
  */
 var dictionary = [String: Int]()
+// or
+var anotherDictionary: [String: Int] = [:]
 /*:
- Notice how the type of the values is Int. The value type can be any type you want. The key can also be any type you want. However, the key has to be a type that provides a value for its `hashValue` property. This is out of the scope of this tutorial, and there are rarely any situations that you will need a custom type to be the key type for a dictionary.
+ Notice how the type of the values is Int. The value type can be any type you want. The key can also be any type you want. However, the key has to conform to the _hashable_ protocol. This has to do with how the dictionary actually works beneath the hood, but that's out of the scope of this tutorial. All the Swift basic types work as a key, and there are rarely any situations that you will need a custom type to be the key anyway.
  */
 /*:
 [Previous](@previous) | [Table of Contents](P00-Table-of-Contents) | [Next](@next)
