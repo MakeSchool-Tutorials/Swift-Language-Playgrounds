042bcc8907f8760b9d7b0bb577c0b8f658b5e765
Daniel Haaser <thedapperdan@gmail.com>
2016-06-06 00:44:08 -0700

    content and formatting changes to P12

--- Swift-Intro.playground/Pages/P12-Initializers.xcplaygroundpage/Contents.swift
+++ Swift-Intro.playground/Pages/P12-Initializers.xcplaygroundpage/Contents.swift
@@ -1,12 +1,11 @@
 /*:
  # Initializers
- We have seen a couple of designated initializers in the Classes tutorial. In this tutorial, we will go more in depth and cover designated, convenience, required, and failable  initializers.
+ There are many ways to skin a cat, and in Swift, there can be many ways to initialize a class. In this tutorial, we will cover _designated_, _convenience_, _required_, and _failable_ initializers. When you're just starting out, you probably won't have a need to use many of these, but as your classes become larger and more complex, using a specialized initializer can help cut down the amount of code you have to write overall.
  */
 /*:
  ## Designated Initializers
- Let's start with designated initializers.
+ Let's start with designated initializers. Designated initializers are the primary initializer for class. In fact, every class must have at least one designated initializer. At the end of the designated initializer, all (non-optional) properties must have been initialized. These should look pretty familiar, as you've seen them before:
  */
-
 class Human {
     // weight in pounds
     var pounds : Double
@@ -26,26 +25,22 @@ let john = Human(kgs: 60)
 
 mike.pounds
 john.pounds
-
 /*:
- Using the second initializer, we are able to make it easier for a person who is more familiar with kilograms to use the class.
+ By creating the second designated initializer, we are able to make it easier for a person who is more familiar with kilograms to use the class. In most situations, you will probably only have one designated initializer. But this serves to demonstrate that it's possible to have many.
  
  What if we subclass `Human`? Can we still use the initializers?
  */
-
 class SuperHuman: Human {
     // Some super human
 }
 
 let hero = SuperHuman(kgs: 100)
 let superhero = SuperHuman(pounds: 250)
-
 /*:
  Even though we haven't written the initializers in `SuperHuman`, it inherited them from `Human`.
  
  What if we want to subclass `Human` but want to add more parameters to the initializers?
  */
-
 class AmazingHuman: Human {
     var name : String
     
@@ -59,38 +54,26 @@ class AmazingHuman: Human {
         super.init(kgs: kgs)
     }
 }
-
 /*:
- Notice how we are assigning
- 
- self.name = name
- 
- before calling
- 
- super.init
- 
- That is because Swift will give you an error if you call `super.init` before assigning all the properties in the class that are not optional.
+ Notice how we are assigning `self.name = name` before calling `super.init()`. This is because Swift will give you an error if you call `super.init()` before assigning all the properties in the class that are not optional.
  
  Try to switch
  
- self.name = name
- super.init(kgs: kgs)
- 
+    self.name = name
+    super.init(kgs: kgs)
  to
  
- super.init(kgs: kgs)
- self.name = name
+    super.init(kgs: kgs)
+    self.name = name
  
  and see what Swift will tell you.
  
  __Make sure you undo this last edit before continuing__.
  */
-
 /*:
- ### Optionals With Initializers
- Let's try using an optional with an initializer.
+ ## Optionals and Initializers
+ Let's see how optional properties interact with initializers. In this case, `otherTalent` is an optional property.
  */
-
 class CoolHuman: Human {
     var talent : String
     var otherTalent : String?
@@ -107,54 +90,66 @@ class CoolHuman: Human {
 }
 
 let cool = CoolHuman(talent: "just cool", otherTalent: "more talent", pounds: 0)
-
 /*:
  We still had to assign `talent` before calling `super.init` since it's a non-optional. However, since `otherTalent` is an optional, we were able to assign it after calling `super.init`.
+ 
+ - experiment:
+ Try commenting out `self.otherTalent = otherTalent`. What happens? Actually, in this case, the compiler doesn't complain that `otherTalent` is never initialized, because it's an optional. So it will just be initialized with the value `nil` instead.
  */
 /*:
  ## Convenience Initializers
- When using multiple initializers, it would be nice to have a way to link them all together. To do that, we can use Convenience Initializers.
+ Often times, particularly if your class has many properties, the users of your class won't want to provide values for every single one of the class's properties when they create a new instance. Often, they'll want to provide values for just a few properties, and are okay with the rest of the properties having default values. You can provide this functionality by creating convenience initializers.
  */
-
-class Room {
-    var area = 0.0
-    var info : String?
-    
-    // designated initializer
-    init(area : Double) {
-        self.area = area
-        let areaAsString = area.description
-        self.info = "I'm a nice room. My area is \(areaAsString)"
-    }
-    
-    // convenience initializer
-    convenience init(width : Double, height : Double) {
-        self.init(area: width * height)
-    }
-    
-    // convenience initializer
-    convenience init(height : Double, base : Double) {
-        self.init(area: (height * base) / 2 )
-    }
+enum MilkFat {
+    case Skim, OnePercent, TwoPercent, Whole
 }
 
-/*:
- Instead of repeating:
- 
- self.area = area
- let areaAsString = area.description
- self.info = "I'm a nice room. My area is \(areaAsString)"
- 
- two more times, we used convenience initializers to make it easier to create new instances of `Room` without having to repeat the code that we have already written in our designated initializer.
- 
- __Note: A convenience initializer must call a designated initializer within the current class by using `self.init`. You can not call `super.init` within a convenience initializer.__
- */
+enum MilkFlavor {
+    case None, Chocolate, Strawberry
+}
 
+class Milk {
+    let flavor: MilkFlavor
+    let fat: MilkFat
+    
+    // designated initializer
+    init(flavor: MilkFlavor, fat: MilkFat) {
+        self.flavor = flavor
+        self.fat = fat
+    }
+    
+    // convenience initializer
+    convenience init(flavor: MilkFlavor) {
+        self.init(flavor: flavor, fat: .TwoPercent)
+    }
+    
+    // convenience initializer
+    convenience init(fat: MilkFat) {
+        self.init(flavor: .None, fat: fat)
+    }
+    
+    // convenience initializer
+    convenience init() {
+        self.init(flavor: .None)
+    }
+}
+/*:
+Because of the provided convenience initializers, someone using the `Milk` class can construct an instance of milk without providing any parameters.
+*/
+let defaultMilk = Milk()
+defaultMilk.flavor
+defaultMilk.fat
+/*:
+ Notice that the default values of `None` and `TwoPercent` were chosen. Also notice that during the initialization sequence, two convenience initializers and the designated initializer were called. The sequence went:
+    
+    convenience init() -> convenience init(flavor) -> designated init(flavor, milkfat)
+ 
+ So convenience initializers can call each other, but they must end by calling a designated initializer. Convenience initializers can only call other initializers within the same class. That is to say, they _cannot_ call superclass initializers.
+ */
 /*:
  ## Required Initializers
  Add the word `required` before an initializer if you want every subclass to implement it.
  */
-
 class Fox {
     var age : Int
     
@@ -169,18 +164,13 @@ class AmazingFox: Fox {
         super.init(age: age)
     }
 }
-
 /*:
- Every subclass of `Fox` must implement
- 
- required init(age : Int)
+ Every subclass of `Fox` must implement `required init(age : Int)`
  */
-
 /*:
  ## Failable Initializers
- Failable initializers set requirements for the parameters passed into an initializer.
+ Failable initializers are special in that they can actually fail to create a new instance. You can use these to set requirements for the parameters passed into an initializer. If the parameters don't meet the requirements, then it will fail to create a new instance. You can tell if a initializer is failable by the question mark `?` after `init`.
  */
-
 class Cow {
     var age: Int?
     
@@ -198,20 +188,16 @@ let oddCow = Cow(age: -5)
 
 niceCow
 oddCow
-
 /*:
  Notice how `oddCow` is `nil` because the passed in age is less than zero.
  
- Note that a Failable initializer returns an optional value; therefor, both `niceCow` and `oddCow` are optional values. You can check this by holding down the "option" key on your keyboard and clicking on `niceCow` or `oddCow` above.
+ Note that a Failable initializer returns an optional value; therefore, both `niceCow` and `oddCow` are optional values. You can check this by holding down the "option" key on your keyboard and clicking on `niceCow` or `oddCow` above.
  */
-
 /*:
- ### Initializers For Structs
- All the initializers covered in this tutorial apply to both classes and structs.
- */
-
-/*:
- For more details on what we covered in this tutorial, please visit [Apple's Initialization guide.](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Initialization.html)
+ ## Struct Initialization
+ All the initializers covered here apply to both classes and structs. Huzzah!
+ 
+ For more details on what we covered in this tutorial, please visit [Apple's initialization guide.](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Initialization.html)
  */
 /*:
 [Previous](@previous) | [Table of Contents](P00-Table-of-Contents) | [Next](@next)
